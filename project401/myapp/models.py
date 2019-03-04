294437f7a7e5b943b0a55ca75c63bcb18b43ffc6
Ittigorn <Boat_z_555@hotmail.com>
2019-03-04 19:07:44 +0700

    change_model

--- project401/myapp/models.py
+++ project401/myapp/models.py
@@ -1,36 +1,16 @@
 from django.db import models
 
 
-# class currentCard2(models.Model):
-	
-# 	idCard=models.CharField(max_length=100)
-# 	typeCard=models.CharField(max_length=50)
-# 	descCard = models.CharField(max_length=500)
-# 	date=models.CharField(max_length=50)
-# 	currenttime=models.CharField(max_length=50)
-
-# class historyCard2(models.Model):
-
-# 	idCard=models.CharField(max_length=100)
-# 	typeCard=models.CharField(max_length=50)
-# 	descCard = models.CharField(max_length=500)
-# 	compare =models.CharField(max_length=50) 
-# 	date=models.CharField(max_length=50)
-# 	currenttime=models.CharField(max_length=50)
-
-
 class timeStamp(models.Model):
-	# id = models.AutoField(db_column='ID', primary_key=True)
-	# dates=models.CharField(max_length=50)
-	# times=models.CharField(max_length=50)
-	# changeReq=models.CharField(max_length=100)
+
 	datetime = models.DateTimeField(auto_now_add=True)
 
 class cardRecord(models.Model):
 
 	idCard=models.CharField(max_length=100)
 	actionCard=models.CharField(max_length=50)
-	# timestampID = models.ForeignKey(timeStamp, db_column='timestampID')
+   	# commentCard =models.CharField(max_length=100)
+   	# descCard =models.CharField(max_length=100)
 	timestamp=models.ForeignKey(timeStamp,on_delete=models.CASCADE)
 
 
